71370a1aff79fdde077ea4a234310937c1b980df
koregindev <85156047+koregindev@users.noreply.github.com>
2022-04-06 19:10:45 +0300

    new commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let steps = 5;
 let my_width1 = 0;
 
 if (window.screen.width <= 727) {
-	setInterval(function() {
+	let myInterval1 = setInterval(function() {
 	text_counter.innerText = steps;
 
 	steps -= 1;
@@ -34,8 +34,50 @@ if (window.screen.width <= 727) {
 
 		close_button1.addEventListener('click', function() {
 			myDiv1.style.display = 'none';
-		})
+		});
+
+		clearInterval(myInterval1);
 	}
 
 	}, 1000);
 }
+
+// скрипт для второго рекламного блока
+let button_text = document.getElementById('close_timer_2');
+let close_button2 = document.getElementById("close_button_item");
+let myDiv2 = document.getElementById("modal_camback_2");
+let close_div = document.getElementById("modal_close_div2");
+
+close_button2.style.display = 'none';
+
+let counters = 10;
+
+let myInterval2 = setInterval(function(){
+	button_text.innerText = counters;
+
+	counters -= 1;
+
+	if (counters <= 0) {
+		clearInterval(myInterval2);
+		button_text.style.display = 'none';
+
+		close_button2.style.display = 'block';
+
+		close_div.addEventListener('click', function() {
+			myDiv2.style.display = 'none';
+		});
+	}
+}, 1000);
+
+
+
+let coords_y = window.screen.height;
+modal_coords = 0;
+
+window.addEventListener('scroll', function(){
+	modal_coords += 40;
+
+	if (modal_coords >= coords_y) {
+		myDiv2.style.display = 'block';
+	}
+});
