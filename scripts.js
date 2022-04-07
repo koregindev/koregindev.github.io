3ca0333b439b763c72a46e071ed7fbf8ee062cf1
koregindev <85156047+koregindev@users.noreply.github.com>
2022-04-07 18:52:53 +0300

    fixed

--- scripts.js
+++ scripts.js
@@ -1,83 +1,86 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+	// код для первого баннера
+	let thumb_one = document.querySelector('.thumbnails_block_1');
+	let thumb_text = document.querySelector('.icon_count');
+	let thumb_exit = document.querySelector('.circle_hide');
+	let thumb_overlay = document.querySelector('.thumbnails_overlay');
+	let thumb_sec = 5;
+	let w = 0;
+	let myIterator;
+	//вызывать первое окно через n секунд
 	if (window.screen.width <= 727) {
-		// для вызова первого модального окна через время
-		let thumbnail_one = document.getElementById('thumbnail_one');
-		let thumbnail_text = document.getElementById('contain_text');
-		let close_thumb_1 = document.getElementById('func_btn_close');
-		let thumb_bar1 = document.getElementById('progbar');
+		setTimeout(function() {
+			thumb_one.style.display = 'block';
 
-		let thumb_bar_width = 0;
-		let my_text_counter = 5;
+			myIterator = setInterval(function(){
+				thumb_text.innerText = thumb_sec;
+				thumb_sec--;
+				thumb_overlay.style.width = w + '%';
+				w+= 20;
+				if (thumb_sec < 0) {
+					clearInterval(myIterator);
 
-		close_thumb_1.style.opacity = 0;
-		thumbnail_one.style.display = 'none'
+					thumb_text.style.display = 'none';
+					thumb_exit.style.opacity = 1;
+				}
 
-		let launch_thumbnail1 = setTimeout(function() {
-
-			thumbnail_one.style.display = 'block';
-
-			if (thumbnail_one.style.display === 'block') {
-				document.body.style.overflow = 'hidden';
-			}
-
-			let myLaunch1 = setInterval(function() {	
-				thumbnail_text.innerText = my_text_counter;
-				my_text_counter--;
-
-				if (my_text_counter < 0) {
-					clearInterval(myLaunch1);
-
-					close_thumb_1.style.opacity = 1;
-
-					thumbnail_text.style.display = 'none';
-
-					close_thumb_1.addEventListener('click', function(){
-						thumbnail_one.style.display = 'none';
-
-						if (thumbnail_one.style.display === 'none') {
-							document.body.style.overflow = "auto";
-						}
-					});
-
-				};
-				
-				thumb_bar1.style.width = thumb_bar_width + "%";
-
-				thumb_bar_width += 20;
-
-				if(thumb_bar_width > 80) {
-					close_thumb_1.style.background = '#FFC44E';
+				if (w > 80) {
+					thumb_exit.style.background += "#FFC44E";
 				}
 
 			}, 1000);
 
+			thumb_exit.addEventListener('click', function() {
+				thumb_one.style.display = 'none';
+			});
 
-		}, 8000);
-
-
+		}, 15000);
 	};
+});
 
-	// код для второго блока
-	let thumbnail_two = document.getElementById('thumbnail_two');
-	let thumbnail_text_two = document.getElementById('thumbnail_two');
-	let x_cords = 0;
-	//высота экрана мобильного
+if (window.screen.width <= 727) {
+	let subnail_div = document.querySelector('.subnails_block_2');
+	let subnail_close_button = document.getElementById('sub_button_item');
+	let subnail_text = document.querySelector('.rect_hide_icon');
+	//значение высоты экрана устройства
 	let clientHeight = window.screen.height;
-	//сколько раз показывать второй блок
+	//переменная содержит значение высоты
+	let my_coords = 0;
+	//новый таймер показа рекламы
+	let myIterator2;
+	//значение секунд до показа рекламы
+	let stepps = 10;
+
 	let views = 1;
 
-	window.addEventListener('scroll', function(){
-
-		setInterval(function(){
-			
-			x_cords = window.pageYOffset;
-
-			if (x_cords >= clientHeight) {
-				thumbnail_two.style.display = 'block';
-			}
-
-		}, 1000);
-
+	window.addEventListener('scroll', function() {
+		if (my_coords > clientHeight && views === 1) {
+			subnail_div.style.display = 'block';
+			views++;
+		}
+		my_coords = window.pageYOffset;
 	});
 
-});
+	subnail_close_button.onclick = function() {
+		subnail_close_button.style.display = 'none';
+		subnail_text.style.display = 'block';
+
+		myIterator2 = setInterval(function() {
+			subnail_text.innerText = stepps;
+			stepps -= 1;
+
+			if (stepps <= 0) {
+				clearInterval(myIterator2);
+				subnail_text.style.display = 'none';
+				subnail_close_button.style.display = 'block';
+
+				subnail_close_button.onclick = function() {
+					subnail_div.style.display = 'none';
+				}
+			};
+
+		}, 1000);
+	};
+};
+
+
